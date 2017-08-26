326871bff53912e686e84bf306205691dc15cd30
DylDelport <31354787+DylDelport@users.noreply.github.com>
2017-08-26 12:23:30 -0700

    Initial File Upload

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+    
+        <title>Ballito Summer Fest</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+        
+        <link rel="stylesheet" type="text/css" href="home.css">
+    </head>
+    
+    <body>
+        
+        <header>
+            <div id="topbar">
+                <div id="home-logo">
+                    <a href=""> <img src="Images/homeicon.png" height="32px" width="32px"></a>
+                </div>
+                
+                <div class="topbar-section">
+                    <a href="" >WELCOME</a>
+                </div>
+                
+                <div class="topbar-section">
+                    <a href="" >TICKETS</a>
+                </div>
+                
+                <div class="topbar-section">
+                    <a href="" >LINEUP</a>
+                </div>
+                
+                <div class="topbar-section">
+                    <a href="" >FAQ</a>
+                </div>
+                
+                <div class="topbar-section">
+                    <a href="" >CONTACT</a>
+                </div>
+                    
+                <div class="topbar-social">
+                    <a href="https://www.facebook.com/ballitosf"><img src="Images/facebook-icon.png" height="32px" width="32px"></a>
+                    <a href="https://www.instagram.com/"><img src="Images/instagram-icon.png" height="32px" width="32px"></a>
+				    <a href="https://www.twitter.com/"><img src="Images/twitter-icon.png" height="32px" width="32px"></a>
+                </div>
+            </div>
+            
+            <div class="header-background">
+                <img src="Images/web%20header-01.jpg" height="360" width="1440">
+            </div>
+            
+            <div class="header-logo">
+                <img src="Images/headerlogo.png" alt="Logo" width=350; height=346 >
+            </div>
+        </header>
+        
+        <div class="clear"></div>
+        
+        <div class="container-blocks">
+        
+            <div class="block--welcome">
+                <a class="block-welcome-image"> <img src="Images/date%20box-01.png" width="400" height="380"></a>
+                <div class="block-welcome-textwrap">
+                    <p>Welcome to the official website of the</p>
+                    
+                    <h1>Ballito Summer Festival</h1>
+                    
+                    <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus urna sapien, vulputate ac feugiat eu, condimentum eu magna. Maecenas volutpat eget erat at molestie. Nam feugiat nulla nulla, ut rhoncus nisl sollicitudin a. Quisque nisl risus, feugiat in malesuada ac, lacinia a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed elit nisl, blandit sed dolor a, convallis porttitor arcu. Donec et lorem in ex euismod mollis. Sed condimentum dapibus aliquam. Integer scelerisque arcu lacinia est rutrum consequat. Nunc rutrum, lorem quis eleifend ullamcorper, risus turpis pretium orci, vitae malesuada lorem tortor id urna. Aenean et ante vitae lacus consequat volutpat eget faucibus arcu. Nunc ante enim, facilisis ornare risus sed, tempor gravida tellus. Curabitur rutrum hendrerit mattis. Vestibulum eget magna suscipit, fermentum mauris nec, volutpat dolor. y<br><br></p1>
+                    
+                    <p2>Click the button below for more info</p2>
+                    
+                    <div class="block-welcome-button">
+                        <a class="more-info" href=""></a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="clear"></div>
+            
+            <div class="block--ticketsales">
+                <a href=""></a>
+                
+            </div>
+            
+            <div class="block--dates">
+                <a></a>
+                
+            </div>
+        
+            <div class="block--artistlineup">
+                <a href=""></a>
+            </div>
+        
+            <div class="block--FAQ">
+                <a href=""></a>      
+            </div>
+        </div>
+        
+        <div class="clear"></div>
+        
+        <div class="container-footer">
+            
+            <div class="social-container">
+                <div class="social-links">
+				    <a href="https://www.facebook.com/ballitosf"><img src="Images/facebook-icon.png" height="52px" width="52px"></a>
+                    <a href="https://www.instagram.com/"><img src="Images/instagram-icon.png" height="52px" width="52px"></a>
+				    <a href="https://www.twitter.com/"><img src="Images/twitter-icon.png" height="52px" width="52px"></a>
+                </div>
+                
+                <div class="newsletter-container">
+                    <form class="newsletter">
+                        <input class="email" placeholder="Enter your e-mail address" type="email">
+                        <input class="submit" placeholder="Subscribe" type="submit" value="Subscribe">
+                    </form>
+                </div>
+            
+            </div>
+            
+            <div class="clear"></div>
+            
+            <div class="navigation-container">
+                <div class="sitemap">
+                    <div class="sitemap-item-border">
+                        <a href="">HOME</a>
+                    </div>
+                    
+                    <div class="sitemap-item-border">
+                        <a href="">INFO</a>
+                    </div>
+                    
+                    <div class="sitemap-item-border">
+                        <a href="">TICKETS</a>
+                    </div>
+                    
+                    <div class="sitemap-item-border">
+                        <a href="">ARTIST LINEUP</a>
+                    </div>
+                    
+                    <div class="sitemap-item-border">
+                        <a href="">FAQ</a>
+                    </div>
+                    
+                    <div class="sitemap-item">
+                        <a href="">CONTACT</a>
+                    </div>
+                </div>
+            </div>
+            
+
+            
+            
+        </div>
+    </body>
+
+    
+</html>
